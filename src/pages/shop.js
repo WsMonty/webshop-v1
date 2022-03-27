769725d516342a197b7ac427fafbf2a3b51c142b
WsMonty <grethen.gilles@gmail.com>
2022-03-27 10:28:32 +0200

    error handling layout component

--- src/pages/shop.js
+++ src/pages/shop.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import Products from './components/products.js';
+import Products from './products.js';
 
 const Shop = () => {
   return (
     <div className="shop">
-      <h1>This is my Shop :)</h1>
       <Products />
     </div>
   );
