5c062a5b2dbd13341c58c68527849b9f27125473
WsMonty <grethen.gilles@gmail.com>
2022-08-05 16:38:47 +0200

    added google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,30 @@ module.exports = {
   },
   plugins: [
     {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingId: 'G-HM13LS731L',
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        exclude: ['/preview/**', '/do-not-track/me/too/'],
+        // Delays sending pageview hits on route update (in milliseconds)
+        pageTransitionDelay: 0,
+        // Defers execution of google analytics script after page load
+        defer: false,
+        // Any additional optional fields
+        sampleRate: 5,
+        siteSpeedSampleRate: 10,
+        // defaults to false
+        enableWebVitalsTracking: true,
+      },
+    },
+    {
       resolve: 'gatsby-source-datocms',
       options: {
         apiToken: '9c970d6f32037d5c733071318563cb',
